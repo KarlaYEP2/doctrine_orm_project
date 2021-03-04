3cbc5d694d6b9eb075c472c68555a35a3f4d3474
Karl Erik <karl-erik.voll@khk.ee>
2021-03-04 18:11:16 +0200

    tagide lisamine ja ette valmistus arvestustöö jaoks

---
+++ src/Entity/Tag.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collection\ArrayCollection;
+
+/**
+* @Entity
+*/
+class Tag {
+	/**
+	* @Column(type="integer")
+	* @Id
+	* @GeneratedValue
+	*/
+	private $id;
+	/**
+	* @Column(type="string")
+	*/
+	private $name;
+
+	/**
+	* @ManyToMany(targetEntity="Article", mappedBy="tags", cascade={"persist"})
+	* @JoinTable(name="article_tags")
+	* @JoinColumn(referencedColumnName="id", nullable=false)
+	*/
+	private $articles;
+
+	public function __construct(){
+		$this->articles = new \Doctrine\Common\Collections\ArrayCollection();
+	}
+
+	public function getId(){
+		return $this->id;
+	}
+
+	public function setId($value){
+		$this->id = $value;
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+
+	public function setName($value){
+		$this->name = $value;
+	}
+
+	public function getArticles(){
+		return $this->articles;
+	}
+}
